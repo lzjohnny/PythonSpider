fed9dae5e80c709dce8c27c60a965c3d8fe5f31c
刘孜健 <lzjohnny@163.com>
2016-08-21 15:10:27 +0800

    完善多线程支持

--- CoreSpider.py
+++ CoreSpider.py
@@ -6,11 +6,13 @@ import HtmlPageDownloader
 import MultiThreadHtmlPageDownloader
 
 class CoreSpider(object):
-    def __init__(self, url):
+    def __init__(self, url, pageDLThreadNum, fileDLThreadNum):
         super().__init__()
         # self.url = url
         self.taskManager = TaskManager.TaskManager()
         self.taskManager.addPageUrl(url)
+        self.pageDLThreadNum = pageDLThreadNum
+        self.fileDLThreadNum = fileDLThreadNum
 
     # 待修改
     def start(self):
@@ -23,18 +25,23 @@ class CoreSpider(object):
         # 多线程下载网页
         print('开始多线程下载页面...')
         htmlParser = HtmlParser.HtmlParser(self.taskManager)
-        pageDownloader_thread1 = MultiThreadHtmlPageDownloader.MultiThreadHtmlPageDownloader(self.taskManager, htmlParser, 1)
-        pageDownloader_thread2 = MultiThreadHtmlPageDownloader.MultiThreadHtmlPageDownloader(self.taskManager, htmlParser, 2)
-        pageDownloader_thread3 = MultiThreadHtmlPageDownloader.MultiThreadHtmlPageDownloader(self.taskManager, htmlParser, 3)
-        pageDownloader_thread4 = MultiThreadHtmlPageDownloader.MultiThreadHtmlPageDownloader(self.taskManager, htmlParser, 4)
-        pageDownloader_thread5 = MultiThreadHtmlPageDownloader.MultiThreadHtmlPageDownloader(self.taskManager, htmlParser, 5)
 
         pageDLthreadsList = []
-        pageDLthreadsList.append(pageDownloader_thread1)
-        pageDLthreadsList.append(pageDownloader_thread2)
-        pageDLthreadsList.append(pageDownloader_thread3)
-        pageDLthreadsList.append(pageDownloader_thread4)
-        pageDLthreadsList.append(pageDownloader_thread5)
+        for i in range(self.pageDLThreadNum):
+            pageDownloaderThread = MultiThreadHtmlPageDownloader.MultiThreadHtmlPageDownloader(self.taskManager, htmlParser, i)
+            pageDLthreadsList.append(pageDownloaderThread)
+
+        # pageDownloader_thread1 = MultiThreadHtmlPageDownloader.MultiThreadHtmlPageDownloader(self.taskManager, htmlParser, 1)
+        # pageDownloader_thread2 = MultiThreadHtmlPageDownloader.MultiThreadHtmlPageDownloader(self.taskManager, htmlParser, 2)
+        # pageDownloader_thread3 = MultiThreadHtmlPageDownloader.MultiThreadHtmlPageDownloader(self.taskManager, htmlParser, 3)
+        # pageDownloader_thread4 = MultiThreadHtmlPageDownloader.MultiThreadHtmlPageDownloader(self.taskManager, htmlParser, 4)
+        # pageDownloader_thread5 = MultiThreadHtmlPageDownloader.MultiThreadHtmlPageDownloader(self.taskManager, htmlParser, 5)
+
+        # pageDLthreadsList.append(pageDownloader_thread1)
+        # pageDLthreadsList.append(pageDownloader_thread2)
+        # pageDLthreadsList.append(pageDownloader_thread3)
+        # pageDLthreadsList.append(pageDownloader_thread4)
+        # pageDLthreadsList.append(pageDownloader_thread5)
 
         for t in pageDLthreadsList:
             t.start()
@@ -51,18 +58,22 @@ class CoreSpider(object):
 
         #多线程下载图片
         # print('开始下载图片')
-        fileDownloader_thread1 = MultiThreadFileDownloader.MultiThreadFileDownloader(self.taskManager, 1)
-        fileDownloader_thread2 = MultiThreadFileDownloader.MultiThreadFileDownloader(self.taskManager, 2)
-        fileDownloader_thread3 = MultiThreadFileDownloader.MultiThreadFileDownloader(self.taskManager, 3)
-        fileDownloader_thread4 = MultiThreadFileDownloader.MultiThreadFileDownloader(self.taskManager, 4)
-        fileDownloader_thread5 = MultiThreadFileDownloader.MultiThreadFileDownloader(self.taskManager, 5)
-
         fileDLthreadsList = []
-        fileDLthreadsList.append(fileDownloader_thread1)
-        fileDLthreadsList.append(fileDownloader_thread2)
-        fileDLthreadsList.append(fileDownloader_thread3)
-        fileDLthreadsList.append(fileDownloader_thread4)
-        fileDLthreadsList.append(fileDownloader_thread5)
+        for i in range(self.fileDLThreadNum):
+            fileDownloaderThread = MultiThreadFileDownloader.MultiThreadFileDownloader(self.taskManager, i)
+            fileDLthreadsList.append(fileDownloaderThread)
+
+        # fileDownloader_thread1 = MultiThreadFileDownloader.MultiThreadFileDownloader(self.taskManager, 1)
+        # fileDownloader_thread2 = MultiThreadFileDownloader.MultiThreadFileDownloader(self.taskManager, 2)
+        # fileDownloader_thread3 = MultiThreadFileDownloader.MultiThreadFileDownloader(self.taskManager, 3)
+        # fileDownloader_thread4 = MultiThreadFileDownloader.MultiThreadFileDownloader(self.taskManager, 4)
+        # fileDownloader_thread5 = MultiThreadFileDownloader.MultiThreadFileDownloader(self.taskManager, 5)
+
+        # fileDLthreadsList.append(fileDownloader_thread1)
+        # fileDLthreadsList.append(fileDownloader_thread2)
+        # fileDLthreadsList.append(fileDownloader_thread3)
+        # fileDLthreadsList.append(fileDownloader_thread4)
+        # fileDLthreadsList.append(fileDownloader_thread5)
 
         for t in fileDLthreadsList:
             t.start()
