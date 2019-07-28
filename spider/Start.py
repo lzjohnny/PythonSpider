f8077657c717707ec795e3a7aed8c629edfbdc15
lzjohnny <lzjohnny@163.com>
2019-07-28 14:08:50 +0800

    路径导入

--- spider/Start.py
+++ spider/Start.py
@@ -1,3 +1,10 @@
+import os
+import sys
+
+curPath = os.path.abspath(os.path.dirname(__file__))
+rootPath = os.path.split(curPath)[0]
+sys.path.append(rootPath)
+
 from spider import CoreSpider
 
 if __name__ == "__main__":
