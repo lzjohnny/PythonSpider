65def9cda6ef03756002b6d71a1229fa6bdb2c49
lzjohnny <lzjohnny@163.com>
2019-07-29 23:55:27 +0800

    增加萌百解析规则

--- spider/Start.py
+++ spider/Start.py
@@ -12,7 +12,9 @@ if __name__ == "__main__":
     # 开始爬虫
     log.info("开始爬虫!")
 
-    # 种子链接：导航页面
+    # 萌百爬虫种子链接：舰C导航页面
+    # url = "https://zh.moegirl.org/zh-cn/Template:%E8%88%B0%E9%98%9FCollection:%E5%AF%BC%E8%88%AA"
+    # 弹吧爬虫种子链接：首个乐谱页面
     url = "http://www.tan8.com/yuepu-10-m.html"
 
     pageDLThreadNum = 10
